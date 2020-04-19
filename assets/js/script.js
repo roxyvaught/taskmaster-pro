ae9c92427765965ebefc1a2e6135ba2c7e4559c9
roxyvaught <roxanna.vaught@gmail.com>
2020-04-19 17:01:33 -0500

    add date picker

--- assets/js/script.js
+++ assets/js/script.js
@@ -148,15 +148,23 @@ $(".list-group").on("click", "span", function() {
     .addClass("form-control")
     .val(date);
 
-  // swap out elements
-  $(this).replaceWith(dateInput);
+    $(this).replaceWith(dateInput);
 
-  // automatically focus on new element
-  dateInput.trigger("focus");
+    // enable jquery ui datepicker
+    dateInput.datepicker({
+      minDate: 1,
+      onClose: function() {
+        // when calendar is closed, force a "change" event on the `dateInput`
+        $(this).trigger("change");
+      }
+    });
+  
+    // automatically bring up the calendar
+    dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
     .val()
@@ -249,4 +257,8 @@ $("#trash").droppable({
   out: function(event, ui) {
     console.log("out");
   }
+});
+
+$("#modalDueDate").datepicker({
+  minDate: 1
 });
