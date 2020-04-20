9b0a41a503e76e341d837f281b4b60b2c19ff90e
roxyvaught <roxanna.vaught@gmail.com>
2020-04-19 19:48:52 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,7 +92,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -205,16 +205,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log("activate", this);
+    $(this).addClass('dropover');
+    $('.bottom-trash').addClass('bottom-trash-drag');
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass('bottom-trash-drag');
   },
   over: function(event) {
-    console.log("over", event.target);
+    $(event.target).addClass('dropover-active');
   },
   out: function(event) {
-    console.log("out", event.target);
+    $(event.target).removeClass('dropover-active');
   },
   update: function(event) {
     // array to store task data in
@@ -269,3 +271,8 @@ $("#modalDueDate").datepicker({
   minDate: 1
 });
 
+setInterval(function () {
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
